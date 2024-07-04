e2f49842c88ea34a6dc0fe6384258427e2026f13
RodCaba <rodser4@gmail.com>
2024-07-03 17:36:29 -0700

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      host: 'oac.j6gsnpx.mongodb.net',
-      
-    })
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService) => ({
+        type: 'mongodb',
+        host: configService.get('DB_HOST'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        appname: configService.get('DB_APPNAME'),
+        autoLoadEntities: true,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
